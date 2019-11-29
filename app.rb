d0b0bc6ba39b092dfa07f88e2ebd95436dd8efe3
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-29 21:52:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,9 +25,6 @@ class App < Sinatra::Base
     @number1 = params[:number1]
     @number2 = params[:number2]
     @operation = params[:operation]
-    "#{@number1 + @number2}"
-    "#{@number1 - @number2}"
-    "#{@number1 * @number2}"
-    #@number1.send(@operation,@number2)
+    @number1.send(@operation,@number2)
   end 
 end 
