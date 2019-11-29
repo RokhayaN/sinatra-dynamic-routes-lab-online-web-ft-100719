5570e972ec68b9c029a73e903ea8517b3e67073d
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-29 21:38:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,10 +19,5 @@ class App < Sinatra::Base
   end
   
   get /say/:word1/:word2/:word3/:word4/:word5
-    @word1=params[]
-    @word2=params[]
-    @word3=params[]
-    @word4=params[]
-    @word5=params[]
-  
+   "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   
