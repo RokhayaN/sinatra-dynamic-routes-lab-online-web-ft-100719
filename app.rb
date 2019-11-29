8248d2656c6d321c0f0ea270f7a09c39317c5009
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-29 21:48:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,6 +27,6 @@ class App < Sinatra::Base
     @operation = params[:operation]
     "#{@number1 + @number2}"
 
-    @number1.send(@operation,@number2)
+    #@number1.send(@operation,@number2)
   end 
 end 
