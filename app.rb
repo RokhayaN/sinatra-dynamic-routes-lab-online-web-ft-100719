200e05af0c028eefc02170c7d83cac1bdee54ec1
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-29 21:37:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,11 +14,15 @@ class App < Sinatra::Base
   
   get '/say/:number/:phrase'do 
     @phrase=params[:phrase]
-    @number=params[:number]
+    @number=params[:number].to_i
     "#{@phrase}\n" * @number
   end
   
   get /say/:word1/:word2/:word3/:word4/:word5
-    :word1/:word2/:word3/:word4/:word5
+    @word1=params[]
+    @word2=params[]
+    @word3=params[]
+    @word4=params[]
+    @word5=params[]
   
   
